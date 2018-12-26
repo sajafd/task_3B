1dbe344cb2fd4e4874c65eaeadc1fb68d3015263
lailalelouch <lsb-95@hotmail.com>
2018-12-26 10:33:59 +0300

    new task

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
 
-def welcome(request):
-    return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
-    context = {
-
-    }
-    return render(request, 'list.html', context)
+	pass
 
 
 def restaurant_detail(request):
-
-    context = {
-
-    }
-    return render(request, 'detail.html', context)
+	pass
